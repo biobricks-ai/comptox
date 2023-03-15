4fe497df0c7cf7ddff736a04a238d4795daf3c99
Thomas Luechtefeld <tom@insilica.co>
2023-03-14 23:27:34 -0400

    added some comments

--- stages/1_download.R
+++ stages/1_download.R
@@ -1,8 +1,9 @@
 library(arrow)
 
-# see https://clowder.edap-cluster.com/datasets/61147fefe4b0856fdc65639b#folderId=61148181e4b0856fdc65643f&page=0
+# TODO review the below files and look for more data:
+# https://clowder.edap-cluster.com/datasets/61147fefe4b0856fdc65639b#folderId=61148181e4b0856fdc65643f&page=0
 
-# Define the FTP base address --> access 11-17-2022
+# dsstox_identifiers maps dsstoxid to casrn and inchi
 stage = fs::dir_create("staging") |> fs::path("dsstox_identifiers.csv")
 urllink="https://clowder.edap-cluster.com/files/616dd943e4b0a5ca8aeea69d/blob"
 download.file(urllink, destfile = stage, mode = "wb")
