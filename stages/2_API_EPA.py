dbd9b0eb93d0962d16d15dd60c015ac961fd175b
ABorrel <a.borrel@gmail.com>
2023-04-21 18:24:52 +0000

    add API request to build chemicals table with list

---
+++ stages/2_API_EPA.py
@@ -0,0 +1,53 @@
+import http.client
+import json
+import pandas as pd
+
+
+def run_EPA_API(x_api_key, url):
+    """
+    Run api with GET method
+    return JSON format
+    """
+    conn = http.client.HTTPSConnection("api-ccte.epa.gov")
+    headers = {
+        'Accept': "*/*",
+        'x-api-key': x_api_key
+    }
+
+
+    # extract the all list of chemicals
+    conn.request("GET", url, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode('utf-8')
+    data_js = json.loads(data)
+
+    return data_js
+
+
+# MAIN #
+########
+p_dir_bricks = "./brick/"
+
+# 1. retrieve all list of chemicals
+x_api_key = "b6808991-0f5e-4019-9512-ea85d80d54fa"
+url_list = "/chemical/list/"
+
+l_d_listChem = run_EPA_API(x_api_key, url_list)
+
+df_list = pd.DataFrame()
+for d_listChem in l_d_listChem:
+    new_row = pd.DataFrame.from_dict(d_listChem, orient='index', columns=['Value']).T
+    df_list = pd.concat([df_list, new_row], ignore_index=True)
+df_list.to_parquet(p_dir_bricks + "/chemical_lists.parquet")
+
+# 2. extract chemicals by list and make a dataframe
+l_list_chem = df_list["name"]
+df_chemical = pd.DataFrame()
+for list_chem in l_list_chem:
+    print("==>", list_chem)
+    l_d_chem_bylist = run_EPA_API(x_api_key, "/chemical/list/chemicals/search/by-listname/%s"%(list_chem))
+    for d_chem_bylist in l_d_chem_bylist:
+        new_row = pd.DataFrame.from_dict(d_chem_bylist, orient='index', columns=['Value']).T
+        df_chemical = pd.concat([df_chemical, new_row], ignore_index=True)
+df_chemical.to_parquet(p_dir_bricks + "/chemicals_by_list.parquet")
